10f3fd84a1d52074c606236350fa6f86e327d4ae
SgaggeroAlessandro <14529841@itisrossi.vi.it>
2026-05-11 11:10:22 +0200

    Aggiungere i file di progetto.

---
+++ DanceDanceRevolution/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DanceDanceRevolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSu = new System.Windows.Forms.Button();
+            this.btnDestra = new System.Windows.Forms.Button();
+            this.btnGiu = new System.Windows.Forms.Button();
+            this.btnSinistra = new System.Windows.Forms.Button();
+            this.btnCentro = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(145, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANCE DANCE REVOLUTION";
+            // 
+            // btnSu
+            // 
+            this.btnSu.BackColor = System.Drawing.Color.White;
+            this.btnSu.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSu.Location = new System.Drawing.Point(270, 58);
+            this.btnSu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSu.Name = "btnSu";
+            this.btnSu.Size = new System.Drawing.Size(72, 58);
+            this.btnSu.TabIndex = 1;
+            this.btnSu.Text = "🠕";
+            this.btnSu.UseVisualStyleBackColor = false;
+            this.btnSu.Click += new System.EventHandler(this.btnSu_Click);
+            // 
+            // btnDestra
+            // 
+            this.btnDestra.BackColor = System.Drawing.Color.White;
+            this.btnDestra.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDestra.Location = new System.Drawing.Point(400, 154);
+            this.btnDestra.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDestra.Name = "btnDestra";
+            this.btnDestra.Size = new System.Drawing.Size(72, 58);
+            this.btnDestra.TabIndex = 2;
+            this.btnDestra.Text = "🠖";
+            this.btnDestra.UseVisualStyleBackColor = false;
+            this.btnDestra.Click += new System.EventHandler(this.btnDestra_Click);
+            // 
+            // btnGiu
+            // 
+            this.btnGiu.BackColor = System.Drawing.Color.White;
+            this.btnGiu.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiu.Location = new System.Drawing.Point(270, 247);
+            this.btnGiu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGiu.Name = "btnGiu";
+            this.btnGiu.Size = new System.Drawing.Size(72, 58);
+            this.btnGiu.TabIndex = 3;
+            this.btnGiu.Text = "🠗";
+            this.btnGiu.UseVisualStyleBackColor = false;
+            this.btnGiu.Click += new System.EventHandler(this.btnGiu_Click);
+            // 
+            // btnSinistra
+            // 
+            this.btnSinistra.BackColor = System.Drawing.Color.White;
+            this.btnSinistra.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSinistra.Location = new System.Drawing.Point(120, 154);
+            this.btnSinistra.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSinistra.Name = "btnSinistra";
+            this.btnSinistra.Size = new System.Drawing.Size(72, 58);
+            this.btnSinistra.TabIndex = 4;
+            this.btnSinistra.Text = "🠔";
+            this.btnSinistra.UseVisualStyleBackColor = false;
+            this.btnSinistra.Click += new System.EventHandler(this.btnSinistra_Click);
+            // 
+            // btnCentro
+            // 
+            this.btnCentro.BackColor = System.Drawing.Color.White;
+            this.btnCentro.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCentro.Location = new System.Drawing.Point(270, 154);
+            this.btnCentro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCentro.Name = "btnCentro";
+            this.btnCentro.Size = new System.Drawing.Size(72, 58);
+            this.btnCentro.TabIndex = 5;
+            this.btnCentro.Text = "⦿";
+            this.btnCentro.UseVisualStyleBackColor = false;
+            this.btnCentro.Click += new System.EventHandler(this.btnCentro_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PUNTEGGIO:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCentro);
+            this.Controls.Add(this.btnSinistra);
+            this.Controls.Add(this.btnGiu);
+            this.Controls.Add(this.btnDestra);
+            this.Controls.Add(this.btnSu);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSu;
+        private System.Windows.Forms.Button btnDestra;
+        private System.Windows.Forms.Button btnGiu;
+        private System.Windows.Forms.Button btnSinistra;
+        private System.Windows.Forms.Button btnCentro;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DanceDanceRevolution/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DanceDanceRevolution
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = new SoundPlayer();
+        Random rnd = new Random();
+        int punteggio = 0;
+        bool statosu = false;
+        bool statodestra = false;
+        bool statogiu = false;
+        bool statosinistra = false;
+        bool statocentro = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            player.SoundLocation = "pumpupthemind-dance-revolution-243729.wav";
+            player.PlayLooping();
+            label2.Text = "Punteggio: " + punteggio;
+            await Task.Delay(3000);
+            generaNumero();
+        }
+        private  void generaNumero()
+        {
+            int n = rnd.Next(1, 6);
+           
+            if (n == 1)
+            {   
+               
+                FrecciaSu();
+            }
+            else if (n == 2)
+            {
+                
+                FrecciaDestra();
+            }
+            else if (n == 3)
+            {
+               
+                FrecciaGiu();
+            }
+            else if (n == 4)
+            {
+               
+                FrecciaSinistra();
+            }
+            else if(n == 5)
+            {
+               
+                Centro();
+            }
+        }
+        private void FrecciaSu()
+        {
+            btnSu.BackColor = Color.Green;
+            statosu = true;
+        }
+
+        private void FrecciaDestra()
+        {
+            btnDestra.BackColor = Color.Yellow;
+            statodestra = true;
+        }
+        private void FrecciaGiu()
+        {
+            btnGiu.BackColor = Color.Blue;
+            statogiu = true;
+        }
+        private void FrecciaSinistra()
+        {
+            btnSinistra.BackColor = Color.Fuchsia;
+            statosinistra = true;
+        }
+        private void Centro()
+        {
+            btnCentro.BackColor = Color.Gold;
+            statocentro = true;
+        }
+
+        private void btnSu_Click(object sender, EventArgs e)
+        {
+            if (statosu == true)
+            {
+                punteggio++;
+                label2.Text = "Punteggio: " + punteggio;
+                btnSu.BackColor = Color.White;
+                statosu = false;
+                generaNumero();
+            }
+            else
+            {
+                if (punteggio > 0)
+                {
+                    punteggio--;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+                else
+                {
+                    punteggio = 0;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+            }
+        }
+
+        private void btnDestra_Click(object sender, EventArgs e)
+        {
+            if (statodestra == true)
+            {
+                punteggio++;
+                label2.Text = "Punteggio: " + punteggio;
+                btnDestra.BackColor = Color.White;
+                statodestra = false;
+                generaNumero();
+            }
+            else
+            {
+                if (punteggio > 0)
+                {
+                    punteggio--;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+                else
+                {
+                    punteggio = 0;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+            }
+        }
+
+        private void btnGiu_Click(object sender, EventArgs e)
+        {
+            if (statogiu == true)
+            {
+                punteggio++;
+                label2.Text = "Punteggio: " + punteggio;
+                btnGiu.BackColor = Color.White;
+                statogiu = false;
+                generaNumero();
+            }
+            else
+            {
+                if (punteggio > 0)
+                {
+                    punteggio--;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+                else
+                {
+                    punteggio = 0;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+            }
+        }
+
+        private void btnSinistra_Click(object sender, EventArgs e)
+        {
+            if (statosinistra == true)
+            {
+                punteggio++;
+                label2.Text = "Punteggio: " + punteggio;
+                btnSinistra.BackColor = Color.White;
+                statosinistra = false;
+                generaNumero();
+            }
+            else
+            {
+                if (punteggio > 0)
+                {
+                    punteggio--;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+                else
+                {
+                    punteggio = 0;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+            }
+        }
+
+        private void btnCentro_Click(object sender, EventArgs e)
+        {
+            if (statocentro == true)
+            {
+                punteggio++;
+                label2.Text = "Punteggio: " + punteggio;
+                btnCentro.BackColor = Color.White;
+                statocentro = false;
+                generaNumero();
+            }
+            else
+            {
+                if (punteggio > 0)
+                { 
+                    punteggio--;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+                else
+                {
+                    punteggio = 0;
+                    label2.Text = "Punteggio: " + punteggio;
+                }
+            }
+        }
+
+        
+    }
+}
